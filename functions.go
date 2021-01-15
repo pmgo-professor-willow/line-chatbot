d59dd01d8efddb9fb099600d591482547edf3926
Ze-Hao Wang (Salmon) <salmon.zh.tw@gmail.com>
2021-01-15 20:38:24 +0800

    support text trigger postback

--- functions.go
+++ functions.go
@@ -11,7 +11,35 @@ import (
 )
 
 // WebhookFunction is base LINE webhook entry
-func WebhookFunction(w http.ResponseWriter, r *http.Request) {
+func WebhookFunction(w http.ResponseWriter, req *http.Request) {
+	// LINE messaging API client
+	var client, _ = linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
+		linebot.WithHTTPClient(&http.Client{}),
+	)
+
+	events, _ := client.ParseRequest(req)
+	for _, event := range events {
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				qs, _ := url.ParseQuery(message.Text)
+				PostbackReply(client, event.ReplyToken, qs)
+			}
+
+		case linebot.EventTypePostback:
+			qs, _ := url.ParseQuery(event.Postback.Data)
+			PostbackReply(client, event.ReplyToken, qs)
+		}
+	}
+
+	fmt.Fprint(w, "ok")
+}
+
+// PostbackReply will reply messages by postback
+func PostbackReply(client *linebot.Client, replyToken string, qs url.Values) {
 	cache := GetCache()
 
 	// Refresh cache about data from cloud.
@@ -22,61 +50,43 @@ func WebhookFunction(w http.ResponseWriter, r *http.Request) {
 		cache.UpdatedAt = time.Now()
 	}
 
-	// LINE messaging API client
-	var client, _ = linebot.New(
-		os.Getenv("LINE_CHANNEL_SECRET"),
-		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
-		linebot.WithHTTPClient(&http.Client{}),
-	)
+	if qs.Get("raidTier") != "" {
+		selectedRaidTier := qs.Get("raidTier")
+		selectedRaidBosses := FilterdRaidBosses(cache.RaidBosses, selectedRaidTier)
+		messages := GenerateRaidBossMessages(selectedRaidBosses, selectedRaidTier)
 
-	events, _ := client.ParseRequest(r)
-	for _, event := range events {
-		if event.Type != linebot.EventTypePostback {
-			break
+		replyMessageCall := client.ReplyMessage(replyToken, messages...)
+
+		if _, err := replyMessageCall.Do(); err != nil {
 		}
+	} else if qs.Get("event") != "" {
+		selectedEventLabel := qs.Get("event")
+		filteredGameEvents := FilterGameEvents(cache.GameEvents, selectedEventLabel)
+		messages := GenerateGameEventMessages(filteredGameEvents)
 
-		qs, _ := url.ParseQuery(event.Postback.Data)
+		replyMessageCall := client.ReplyMessage(replyToken, messages...)
 
-		if qs.Get("raidTier") != "" {
-			selectedRaidTier := qs.Get("raidTier")
-			selectedRaidBosses := FilterdRaidBosses(cache.RaidBosses, selectedRaidTier)
-			messages := GenerateRaidBossMessages(selectedRaidBosses, selectedRaidTier)
+		if _, err := replyMessageCall.Do(); err != nil {
+		}
+	} else if qs.Get("graphics") != "" && qs.Get("tweetId") == "" {
+		selectedTwitterUser := qs.Get("graphics")
+		selectedUserTweets := FindUserTweets(cache.TweetList, selectedTwitterUser)
+		messages := GenerateGraphicCatalogMessages(selectedUserTweets)
 
-			replyMessageCall := client.ReplyMessage(event.ReplyToken, messages...)
+		replyMessageCall := client.ReplyMessage(replyToken, messages...)
 
-			if _, err := replyMessageCall.Do(); err != nil {
-			}
-		} else if qs.Get("event") != "" {
-			selectedEventLabel := qs.Get("event")
-			filteredGameEvents := FilterGameEvents(cache.GameEvents, selectedEventLabel)
-			messages := GenerateGameEventMessages(filteredGameEvents)
+		if _, err := replyMessageCall.Do(); err != nil {
+		}
+	} else if qs.Get("graphics") != "" && qs.Get("tweetId") != "" {
+		selectedTwitterUser := qs.Get("graphics")
+		selectedTweetID := qs.Get("tweetId")
+		selectedUserTweets := FindUserTweets(cache.TweetList, selectedTwitterUser)
+		selectedTweet := FindTweet(selectedUserTweets.Tweets, selectedTweetID)
+		messages := GenerateGraphicDetailMessages(selectedTweet, selectedTwitterUser)
 
-			replyMessageCall := client.ReplyMessage(event.ReplyToken, messages...)
+		replyMessageCall := client.ReplyMessage(replyToken, messages...)
 
-			if _, err := replyMessageCall.Do(); err != nil {
-			}
-		} else if qs.Get("graphics") != "" && qs.Get("tweetId") == "" {
-			selectedTwitterUser := qs.Get("graphics")
-			selectedUserTweets := FindUserTweets(cache.TweetList, selectedTwitterUser)
-			messages := GenerateGraphicCatalogMessages(selectedUserTweets)
-
-			replyMessageCall := client.ReplyMessage(event.ReplyToken, messages...)
-
-			if _, err := replyMessageCall.Do(); err != nil {
-			}
-		} else if qs.Get("graphics") != "" && qs.Get("tweetId") != "" {
-			selectedTwitterUser := qs.Get("graphics")
-			selectedTweetID := qs.Get("tweetId")
-			selectedUserTweets := FindUserTweets(cache.TweetList, selectedTwitterUser)
-			selectedTweet := FindTweet(selectedUserTweets.Tweets, selectedTweetID)
-			messages := GenerateGraphicDetailMessages(selectedTweet, selectedTwitterUser)
-
-			replyMessageCall := client.ReplyMessage(event.ReplyToken, messages...)
-
-			if _, err := replyMessageCall.Do(); err != nil {
-			}
+		if _, err := replyMessageCall.Do(); err != nil {
 		}
 	}
-
-	fmt.Fprint(w, "ok")
 }
